c83cb050db165c3270dbd11bccc8f52dce7b659b
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-01-30 19:53:43 +0900

    Startt

--- sgcc_front/src/App.js
+++ sgcc_front/src/App.js
@@ -1,5 +1,12 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import PasswordRecovery from './pages/Login/PasswordRecovery';
+import MyPage from './pages/MyPage/Mypage';
+
+
 
 import './App.css';
 
@@ -7,9 +14,13 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        {/* <Routes>
-          /*<Route path='/' element={<Main />} />
-        </Routes> */}
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/passwordRecovery' element={<PasswordRecovery />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/mypage' element={<MyPage />} />
+        </Routes>
       </div>
     </Router>
   );
