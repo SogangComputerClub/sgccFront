ce81c7f4cf851d5540968c122ada8784579f3294
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-01-30 13:29:39 +0900

    cleaning up

--- sgcc_front/src/App.js
+++ sgcc_front/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-2xl font-bold">
-      Tailwind CSS 설정 완료! 🚀
-    </div>
+    <Router>
+      <div className='App'>
+        {/* <Routes>
+          /*<Route path='/' element={<Main />} />
+        </Routes> */}
+      </div>
+    </Router>
   );
 }
 
